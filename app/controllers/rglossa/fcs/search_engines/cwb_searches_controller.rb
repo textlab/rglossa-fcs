3b9229b069027657d4ca03c20cabbfa75250cab8
Anders Nøklestad <anders.noklestad@iln.uio.no>
2013-09-04 19:45:13 +0200

    Add controller method that dispatches on SRU operation

--- app/controllers/rglossa/fcs/search_engines/cwb_searches_controller.rb
+++ app/controllers/rglossa/fcs/search_engines/cwb_searches_controller.rb
@@ -2,6 +2,23 @@ module Rglossa
   module Fcs
     module SearchEngines
       class CwbSearchesController < Rglossa::SearchEngines::CwbSearchesController
+
+        # Dispatches to the correct method of the RESTful controller we're
+        # inheriting from
+        def disp
+          case params[:operation]
+          when 'searchRetrieve' then perform_create
+          else render text: 'No recognizable operation provided', status: :unprocessable_entity
+          end
+        end
+
+        #########
+        private
+        #########
+
+        def perform_create
+          create
+        end
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rglossa::Engine.routes.draw do
   namespace :fcs do
-    namespace :search_engines do
-      resources :cwb_searches do
-        # SRU queries can use either GET or POST
-        get 'index'
-        post 'index'
-      end
-    end
+    # SRU queries may use either GET or POST
+    # E.g. http://localhost:3000/fcs/cwb/bokmal?version=1.2&operation=searchRetrieve&query=han&maximumRecords=1
+    match 'cwb/:corpusShortName', to: 'search_engines/cwb_searches#disp', via: [:get, :post]
   end
 end
