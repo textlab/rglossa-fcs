56372c6d68e56584eb820e9987e5c09b492fd890
Anders Nøklestad <anders.noklestad@iln.uio.no>
2013-09-04 18:24:07 +0200

    Allow POST as well as GET for searching

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rglossa::Engine.routes.draw do
   namespace :fcs do
     namespace :search_engines do
       resources :cwb_searches do
+        # SRU queries can use either GET or POST
         get 'index'
+        post 'index'
       end
     end
   end
