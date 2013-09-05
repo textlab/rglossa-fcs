2a11a60627949bf874f65bd578a3bd0aae21fab4
Anders Nøklestad <anders.noklestad@iln.uio.no>
2013-09-05 03:20:49 +0200

    Implement the create action of the CwbSearchesController

--- app/controllers/rglossa/fcs/search_engines/cwb_searches_controller.rb
+++ app/controllers/rglossa/fcs/search_engines/cwb_searches_controller.rb
@@ -7,7 +7,7 @@ module Rglossa
         # inheriting from
         def disp
           case params[:operation]
-          when 'searchRetrieve' then perform_create
+          when 'searchRetrieve' then create
           else render text: 'No recognizable operation provided', status: :unprocessable_entity
           end
         end
@@ -16,8 +16,34 @@ module Rglossa
         private
         #########
 
-        def perform_create
-          create
+        def create
+          query = params[:query]
+
+          # Just make it work with a single query word for now
+          unless query =~ /^".+"$/
+            query = %Q{"#{query}"}
+          end
+          search_params = {
+            queries: [{
+              'query' => query,
+              'corpusShortName' => params[:corpusShortName]
+            }],
+          }
+          @search = create_search(search_params)
+
+          builder = Builder::XmlMarkup.new
+          xml = builder.fsc(:DataView, type: 'application/x-clarin-fcs-kwic+xml') do |v|
+            v.kwic(:kwic, 'xmlns:kwic' => 'http://clarin.eu/fcs/1.0/kwic') do |k|
+              @search.get_result_page(1).each do |hit|
+                s_unit = hit.sub(/^\s*\d+:\s*<s_id.+>:\s*/, '')  # remove position and s ID
+                left, keyword, right = s_unit.match(/(.+)<(.+)>(.+)/)[1..-1].map { |s| s.strip }
+                k.kwic(:c, type: 'left') { |l| l << left }
+                k.kwic(:kw) { |k| k << keyword }
+                k.kwic(:c, type: 'right') { |l| l << right }
+              end
+            end
+          end
+          render xml: xml
         end
       end
     end
