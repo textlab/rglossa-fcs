f9a9d13726ba3a0783415f1cb1b0384a6968de61
Anders Nøklestad <anders.noklestad@iln.uio.no>
2013-09-05 22:00:04 +0200

    XML template for CWB results

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rglossa::Engine.routes.draw do
   namespace :fcs do
     # SRU queries may use either GET or POST
     # E.g. http://localhost:3000/fcs/cwb/bokmal?version=1.2&operation=searchRetrieve&query=han&maximumRecords=1
-    match 'cwb/:corpusShortName', to: 'search_engines/cwb_searches#disp', via: [:get, :post]
+    match 'cwb/:corpusShortName', to: 'search_engines/cwb_searches#disp',
+      via: [:get, :post], format: 'xml'
   end
 end
