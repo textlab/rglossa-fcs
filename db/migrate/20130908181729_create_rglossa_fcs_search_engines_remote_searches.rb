3db1b010a61b59cefa38d8b637e9c63ade58aeaf
Anders Nøklestad <anders.noklestad@iln.uio.no>
2014-09-16 23:47:41 +0200

    Remove unused migration

--- db/migrate/20130908181729_create_rglossa_fcs_search_engines_remote_searches.rb
+++
@@ -1,8 +0,0 @@
-class CreateRglossaFcsSearchEnginesRemoteSearches < ActiveRecord::Migration
-  def change
-    create_table :rglossa_fcs_search_engines_remote_searches do |t|
-
-      t.timestamps
-    end
-  end
-end
